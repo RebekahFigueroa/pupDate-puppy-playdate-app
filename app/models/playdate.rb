de110a4100acc03ff941a7ec9b567d4ff1efc9cd
rebekahfigueroa <figueroarebekah@gmail.com>
2024-01-25 20:51:10 -0600

    added seed data

--- app/models/playdate.rb
+++ app/models/playdate.rb
@@ -6,6 +6,7 @@ class Playdate < ApplicationRecord
 
   validates :location, presence: true
   validates :time, presence: true
+  validates :date, presence: true
   validates :size_limit, presence: true
   validates :age_limit, presence: true
   validates :playdate_size_limit, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 2, less_than_or_equal_to: 100 }
--- db/migrate/20240125192333_create_playdates.rb
+++ db/migrate/20240125192333_create_playdates.rb
@@ -4,6 +4,7 @@ class CreatePlaydates < ActiveRecord::Migration[6.1]
       t.references :owner_id, null: false, foreign_key: true
       t.string :location
       t.timestamp :time
+      t.timestamp :date
       t.string :size_limit
       t.string :age_limit
       t.integer :playdate_size_limit
